a7633c1d467f22202fa6028b9347acdd85e2eccc
Maleah Walden <maleah13walden@gmail.com>
2020-10-30 14:41:52 -0500

    Completed 1-4

---
+++ functionsExercise.js
@@ -0,0 +1,25 @@
+ function spookyGhost(){
+  console.log(`Boo!!!`);
+}
+spookyGhost();
+console.log(`=========================`);
+
+function candy(goal){
+  console.log(`${goal}, ${goal}, ${goal}, ${goal}`);
+}
+candy(`Get Candy`);
+console.log(`=========================`);
+
+
+// function werewolf(a, b){
+//   console.log(`${a} Full ${a - b} Moon ${b}`);
+// }
+// werewolf(`ARH-WOOO!`);
+
+function halloween(){
+  return `October 31st`;
+}
+let day = halloween();
+console.log(day);
+console.log(`=========================`);
+
