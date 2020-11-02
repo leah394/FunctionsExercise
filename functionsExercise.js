5ff09cac474049a145231f93673c5ac3341a8f66
Maleah Walden <maleah13walden@gmail.com>
2020-11-02 12:51:07 -0600

    Completed

--- functionsExercise.js
+++ functionsExercise.js
@@ -11,10 +11,13 @@ candy(`Get Candy`);
 console.log(`=========================`);
 
 
-// function werewolf(a, b){
-//   console.log(`${a} Full ${a - b} Moon ${b}`);
-// }
-// werewolf(`ARH-WOOO!`);
+function werewolf(a, b){
+  if (a === "Full" && b === "Moon"){
+  console.log("ARH-WOOO!");
+  }
+}
+werewolf("Full", "Moon");
+console.log(`=========================`);
 
 function halloween(){
   return `October 31st`;
@@ -23,3 +26,40 @@ let day = halloween();
 console.log(day);
 console.log(`=========================`);
 
+function isEnoughCandy(pieces){
+  if (pieces >= 31){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEnoughCandy(31));
+console.log(`=========================`);
+
+function candyCounter(num){
+  let total = 0;
+  for (n of num){
+    total = total + n;
+  }
+  return total;
+}
+candyCounter([2, 3, 4, 5]);
+candyCounter([2, 3, 4, 5, 6, 6]);
+
+const hauntedMansion = function(){
+  console.log(`Welcome, foolish mortals, to the Haunted Mansion!`);
+}
+
+const ghostbusters = () => (`Who You Gonna Call?`);
+console.log(ghostbusters());
+console.log(`=========================`);
+
+// 9
+// No
+
+// 10
+// Yes
+
+const costumes = [`Ghost`, `Hobo`, `Superman`];
+const upperCostumes = costumes.map(costumes => costumes.toUpperCase());
+console.log(upperCostumes);
